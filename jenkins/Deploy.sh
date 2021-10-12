16aba4a896dd4823e469ad8bd5eefae4eaf59948
Alvis Aung <alvisaung@gmail.com>
2021-10-12 10:52:35 +0630

    s

--- jenkins/Deploy.sh
+++ jenkins/Deploy.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env sh
 
 echo "Can' figure out to replace localhost with ec2 ip address. ;-("
-set -x
 npm run build
-set +x
-
 echo 'The following "npm" command runs your Node.js/React application in'
 
-set -x
-npm start &
-sleep 1
-echo $! > .pidfile
-set +x
+npm start 
 
 echo 'Now...'
